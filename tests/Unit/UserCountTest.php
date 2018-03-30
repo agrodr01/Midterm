9ae79e69f5ca05f688ad07e9ccad0a4030632f47
dylan <dylanagr91@gmail.com>
2018-03-29 23:54:06 -0400

    CountUser - Still couldnt fingure these out

---
+++ tests/Unit/UserCountTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserCountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $userCount = 50;
+        $this->assertTrue(true);
+    }
+}
